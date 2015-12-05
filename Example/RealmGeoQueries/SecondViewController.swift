3d39be8aec1ca64d0b7ed67cb41de7ebefd67561
mhergon <mhergon@gmail.com>
2015-12-05 15:01:50 +0100

    First commit

---
+++ Example/RealmGeoQueries/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RealmGeoQueries
+//
+//  Created by mhergon on 5/12/15.
+//  Copyright © 2015 Marc Hervera. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
