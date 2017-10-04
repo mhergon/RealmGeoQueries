66307419cf8aa75b141e016cf07837e0a7c1d2ec
Marc Hervera <mhergon@gmail.com>
2017-10-04 13:31:59 +0200

    * Carthage and CocoaPods config

---
+++ GeoQueries/GeoQueries.h
@@ -0,0 +1,19 @@
+//
+//  GeoQueries.h
+//  GeoQueries
+//
+//  Created by mhergon on 4/10/17.
+//  Copyright © 2017 mhergon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoQueries.
+FOUNDATION_EXPORT double GeoQueriesVersionNumber;
+
+//! Project version string for GeoQueries.
+FOUNDATION_EXPORT const unsigned char GeoQueriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoQueries/PublicHeader.h>
+
+
