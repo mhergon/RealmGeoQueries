e279135e19250b7711a1afe96ed44cbbc7fb4f19
Marc Hervera <mhergon@gmail.com>
2020-01-09 17:21:31 +0100

    - Added support for SPM

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RealmGeoQueries",
+    products: [
+        .library(
+            name: "RealmGeoQueries",
+            targets: ["RealmGeoQueries"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "4.3.0"),
+    ],
+    targets: [
+        .target(
+            name: "RealmGeoQueries",
+            dependencies: ["RealmSwift"],
+            path: ".",
+            sources: ["GeoQueries"]
+        )
+    ]
+)
