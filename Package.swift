f7d5a0afcdf757528dee9b02c45347e1a32159ab
Marc Hervera <mhergon@gmail.com>
2020-01-09 17:25:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
         .target(
             name: "RealmGeoQueries",
             dependencies: ["RealmSwift"],
-            path: ".",
             sources: ["GeoQueries"]
         )
     ]
